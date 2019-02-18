bee020e89422292b86ed802687b931dee4645dc3
stefaneidelloth <stefan.eidelloth@posteo.de>
2019-02-18 10:29:05 +0100

    removed javafx example app

--- startTreez.bat
+++ startTreez.bat
@@ -1,5 +1,5 @@
-start cmd /c explorer d:\treezjs
-start cmd /c "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+REM start cmd /c explorer d:\treezjs
+start cmd /c "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/
 d:
 cd d:\treezjs
-http-server
+D:\EclipseJava\App\jdk\bin\java.exe -jar treezServer.jar
--- startTreezApp.bat
+++
@@ -1,5 +0,0 @@
-REM start cmd /c explorer d:\treezjs
-REM start cmd /c "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/
-d:
-cd d:\treezjs
-D:\EclipseJava\App\jdk\bin\java.exe -jar treezApp.jar
