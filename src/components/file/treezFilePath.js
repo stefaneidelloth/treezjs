9e7503444d57e7ad120959a1573d2008fd9ce835
stefaneidelloth <stefan.eidelloth@posteo.de>
2019-07-17 12:12:36 +0200

    improved titles

--- src/components/file/treezFilePath.js
+++ src/components/file/treezFilePath.js
@@ -42,7 +42,7 @@ export default class TreezFilePath extends TreezAbstractPath {
 		    this.__browseButton = browseButton;	
 		    browseButton.className='treez-file-path-browse-button';					   
 		    browseButton.type='button';
-		    browseButton.title=' ';
+		    browseButton.title='Select file path';
 		    browseButton.style.background = 'url("' + urlPrefix + '/icons/browse.png")';
 		    browseButton.style.backgroundRepeat = 'no-repeat';
 		    browseButton.onclick = ()=>this.__browseFilePath();				   
@@ -52,7 +52,7 @@ export default class TreezFilePath extends TreezAbstractPath {
             this.__executeButton = executeButton;
             executeButton.className='treez-file-path-play-button';	
             executeButton.type = 'button';
-            executeButton.title = 'execute';
+            executeButton.title = 'Open file with system editor/executor';
             executeButton.style.background = 'url("' + urlPrefix + '/icons/run_triangle.png")';
             executeButton.style.backgroundRepeat = 'no-repeat';
             executeButton.onclick = ()=>this.execute();   
