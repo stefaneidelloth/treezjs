f66f5e1e995ae5d4f0f724da87e0fa8a1c28613c
stefaneidelloth <stefan.eidelloth@posteo.de>
2019-10-16 14:19:51 +0200

    fixed enum names

--- src/study/studyInfoExport/studyInfoExportType.js
+++ src/study/studyInfoExport/studyInfoExportType.js
@@ -1,13 +1,15 @@
 import Enum from './../../components/enum.js';
 
-export default class StudyInfoExportType extends Enum {}    
+export default class StudyInfoExportType extends Enum {
+
+}
 
 if(window.StudyInfoExportType){
 	StudyInfoExportType = window.StudyInfoExportType;
 } else {
-	StudyInfoExportType.textFile = new StudyInfoExportType('Text file *.txt');                    
-	StudyInfoExportType.sqLite = new StudyInfoExportType('SqLite database *.sqlite');                    
-	StudyInfoExportType.mySql = new StudyInfoExportType('MySQL database');  
+	StudyInfoExportType.textFile = new StudyInfoExportType('textFile');
+	StudyInfoExportType.sqLite = new StudyInfoExportType('sqLite');
+	StudyInfoExportType.mySql = new StudyInfoExportType('mySql');
 	
 	window.StudyInfoExportType = StudyInfoExportType;
 }
