207e5872eb0fa6d11f7cd52daca4d4bcd5e9aa48
stefaneidelloth <stefan.eidelloth@posteo.de>
2019-11-06 17:17:16 +0100

    fixed comment and added image

--- src/study/sensitivity/sensitivityType.js
+++ src/study/sensitivity/sensitivityType.js
@@ -15,8 +15,8 @@ if(window.RelationType){
 	//points are away, e.g. the distance is +-10 % of the absolute working point value => (p-p*0.1), {p}, (p+p*0.1)
 	SensitivityType.relativeDistance = new SensitivityType('relativeDistance');
 	
-	//Specify variation using distances of neighboring points to the working point {p} : specify "how far" the other
-	//points are away, e.g.the absolute distance is 1 => (p-1), {p}, (p+1)
+	//Specify variation using positions of neighboring points in relation to the working point {p} : specify "where" 
+	//the other points are, e.g.the position is 90 %, 110 % of the absolute working point value => (0.9*p), {p}, (1.1*p)
 	SensitivityType.relativePosition = new SensitivityType('relativePosition');
 	
 	//Specify variation using absolute distance of neighboring points e.g. -5, 10 for a working point value of {p=10}
